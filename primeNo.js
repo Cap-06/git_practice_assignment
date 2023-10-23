8dd645c8ef6262cd80f2f8db220dfc33b6428e31
Kunj pratap singh <kunjpratapsingh790@gmail.com>
2023-10-23 11:50:49 +0530

    code is fixed

--- primeNo.js
+++ primeNo.js
@@ -1,11 +1,11 @@
 // Check whether a number is Prime or not..
 
 let num = 6;
- let count = 0;
+let count = 0;
  for(let i=1; i<=num; i++){
-    let count = 0;
     if(num%i==0){
         count++;
+
     }
   }
   if(count==2){
@@ -13,3 +13,4 @@ let num = 6;
   }else{
     console.log("Not a prime Number");
  }
+ 
